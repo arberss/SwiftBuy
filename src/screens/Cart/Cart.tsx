7342b90c96eb360ee70e69e71d342bfa0ba039a2
arberss <arberssalihuu@gmail.com>
2023-11-10 21:29:34 +0100

    added filter functionality

--- src/screens/Cart/Cart.tsx
+++ src/screens/Cart/Cart.tsx
@@ -1,5 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import Header from '@/components/Header/Header';
 import ListCard from '@/components/Cards/ListCard';
 import { FlashList } from '@shopify/flash-list';
--- src/screens/Home/components/ProductList.tsx
+++ src/screens/Home/components/ProductList.tsx
@@ -1,20 +1,24 @@
 import { CartFilledIcon, CartIcon } from '@/assets/SvgIcons';
 import ProductCard from '@/components/Cards/ProductCard';
-import { PRODUCTS } from '@/mockData/products';
-import { useCart } from '@/store/useCart';
+import EmptyListText from '@/components/EmptyListText/EmptyListText';
+import { IProduct, useCart } from '@/store/useCart';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { FlashList } from '@shopify/flash-list';
 import { View } from 'react-native';
 
-const ProductList = () => {
+interface ProductListProps {
+  data: IProduct[];
+}
+
+const ProductList = ({ data }: ProductListProps) => {
   const navigation = useNavigation<StackNavigationProp<any>>();
   const { cart, setInCart } = useCart((state) => state);
 
   return (
     <View style={{ flex: 1 }}>
       <FlashList
-        data={PRODUCTS}
+        data={data}
         extraData={cart.length}
         renderItem={({ item, index }) => {
           const checkCart = cart.find((cartItem) => cartItem.id === item.id);
@@ -38,6 +42,7 @@ const ProductList = () => {
             />
           );
         }}
+        ListEmptyComponent={<EmptyListText text='No products in list' />}
         estimatedItemSize={10}
         showsVerticalScrollIndicator={false}
       />
